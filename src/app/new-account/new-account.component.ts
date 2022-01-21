f5f90f23b347761c131d35bae6e2098fd2eaae22
secretMoi <win.moi@hotmail.com>
2022-01-21 15:16:12 +0100

    Create data service for accounts

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,24 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import {LoggingService} from "../logging.service";
+import {AccountService} from "../account.service";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService] // signale à Angular de fournir ce service par DI
+  providers: [LoggingService, AccountService] // signale à Angular de fournir ce service par DI
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   // injection de dépendance
-  constructor(private loggingService: LoggingService) {
-  }
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService,
+    ) {  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
